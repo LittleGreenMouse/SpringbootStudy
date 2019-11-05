5c06ad632cb3a0b79678f77150dcd99cc7c86612
LittleMouse <haozhang9899@gmail.com>
2019-11-05 15:46:45 +0800

    listener filter and interceptor

--- src/main/java/cn/littlegreenmouse/hello/HelloApplication.java
+++ src/main/java/cn/littlegreenmouse/hello/HelloApplication.java
@@ -3,9 +3,11 @@ package cn.littlegreenmouse.hello;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.littlegreenmouse.hello.generator"})
+@ServletComponentScan
 public class HelloApplication {
 
     public static void main(String[] args) {
